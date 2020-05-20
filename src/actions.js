4a4d7bbb9d38937298ffac3832d2f95dc21690e9
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-05-20 13:40:56 +0530

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -11,7 +11,7 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => (dispatch) =>{
 	dispatch({ type: REQUEST_ROBOTS_PENDING})
-	fetch('http://jsonplaceholder.typicode.com/users')
+	fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => {
 				return response.json();
 			})
