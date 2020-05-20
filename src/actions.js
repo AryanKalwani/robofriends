da8b1206f1e0ecac062b685c3eee3d60e5beb6ed
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-05-20 14:34:22 +0530

    fix

--- src/actions.js
+++ src/actions.js
@@ -11,7 +11,7 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => (dispatch) =>{
 	dispatch({ type: REQUEST_ROBOTS_PENDING})
-	fetch('https://jsonplaceholder.typicode.com/users',{mode:'no-cors'})
+	fetch('http://jsonplaceholder.typicode.com/users')
 			.then(response => {
 				return response.json();
 			})
